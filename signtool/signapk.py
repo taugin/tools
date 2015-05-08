8de578a2abf3e9b245acba797e1f4307f614d728
taugin <taugin@163.com>
2015-05-08 21:35:51 +0800

    Add signapk.py

---
+++ signtool/signapk.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# coding: UTF-8
+import os
+import sys
+import getopt
+import platform
+import subprocess;
+
+seperater = "/"
+winrar = "C:\Program Files (x86)\WinRAR\WINRAR.exe"
+keytool = "keytool"
+jarsigner="jarsigner"
+JDK7ARG="-tsa https://timestamp.geotrust.com/tsa -digestalg SHA1 -sigalg MD5withRSA"
+
+def log(str, show=False):
+    if (show):
+        print(str)
+def inputvalue(prompt, max) :
+    while True:
+        p = input(prompt)
+        if (p.isdigit()):
+            if (int(p) >= 1 and int(p) <= max):
+                return p
+    
+def deletemetainf(src_apk):
+    log("[Logging...] %s" % src_apk)
+    log("[Logging...] Delete META-INF...", True)
+    osstr = platform.system().lower()
+    zipdelete = []
+    if (osstr == "windows"):
+        zipdelete = [winrar, "rd", src_apk, "META-INF"]
+    elif (osstr == "linux"):
+        zipdelete = ["zip", "-dq", src_apk, "META-INF"]
+    else:
+        log("[Logging...] 无法在%s操作系统上运行")
+        sys.exit()
+    subprocess.call(zipdelete, stdout=subprocess.PIPE)
+
+def sign_apk(src_apk, dst_apk, keystoreinfo):
+    log("[Signing...] %s -> %s" % (os.path.basename(src_apk), os.path.basename(dst_apk)), True)
+    deletemetainf(src_apk)
+    if (len(keystoreinfo) <= 0):
+        log ("[Logging...] Sign the apk using test key", True)
+        #deletemetainf "$1"
+        toodir = os.path.dirname(sys.argv[0])
+        signapk_jar = toodir + seperater + "signapk.jar"
+        x509 = toodir + seperater + "testkey.x509.pem"
+        pk8 = toodir + seperater + "testkey.pk8"
+        retcode = subprocess.call(["java", "-jar", signapk_jar, x509, pk8, src_apk, dst_apk], stdout=subprocess.PIPE)
+        if (retcode == 0):
+            log("[Signing...] 签名成功", True)
+        else:
+            log("[Signing...] 签名失败", True)
+    else:
+        log("[Logging...] Sign the apk using keystore : [%s], storepass : [%s] , keyalias : [%s], keypass : [%s]" % (keystoreinfo[0],keystoreinfo[1], keystoreinfo[2], keystoreinfo[3]), True)
+
+        dir = os.path.dirname(src_apk)
+        cmdlist = []
+        cmdlist.append(jarsigner)
+
+        cmdlist.append("-tsa")
+        cmdlist.append("https://timestamp.geotrust.com/tsa")
+        cmdlist.append("-digestalg")
+        cmdlist.append("SHA1")
+        cmdlist.append("-sigalg")
+        cmdlist.append("MD5withRSA")
+        
+        cmdlist.append("-keystore")
+        cmdlist.append(dir + seperater + keystoreinfo[0])
+        cmdlist.append("-storepass")
+        cmdlist.append(keystoreinfo[1])
+        cmdlist.append("-keypass")
+        cmdlist.append(keystoreinfo[3])
+        cmdlist.append("-signedjar")
+        cmdlist.append(dst_apk)
+        cmdlist.append(src_apk)
+        cmdlist.append(keystoreinfo[2])
+        retcode = subprocess.call(cmdlist)
+        if (retcode == 0):
+            log("[Signing...] 签名成功", True)
+        else:
+            log("[Signing...] 签名失败", True)
+            
+    log("", True);
+def exec_sign_process(src_apk, testSign):
+    log("exec_sign_process")
+    index = src_apk.rfind(".apk")
+    if (index == -1):
+        log("[Error...] 无法识别的的apk压缩包", True)
+        sys.exit()
+    log("index : %d " % index)
+    log("substring : %s " % src_apk[0:index])
+    dst_apk = src_apk[0:index] + "-signed.apk"
+    log("dst_apk : %s " % dst_apk)
+    keystoreinfo = []
+    if(testSign == False):
+        keystoreinfo = readkeystore(os.path.dirname(src_apk))
+    sign_apk(src_apk, dst_apk, keystoreinfo)
+
+def readkeystore(dir):
+    listfile=os.listdir(dir)
+    storefiles = []
+    #log(listfile)
+    index = 0
+    storeindex = 0
+    for file in listfile:
+        if(file.rfind(".keystore") != -1):
+            storefiles.append(file)
+            storeindex+=1
+            log("             [%d] : %s" %(storeindex, file), True)
+        index+=1
+    if (storeindex > 1):
+        p = inputvalue("请输入签名文件索引(1-%d) : "%storeindex, storeindex)
+    else:
+        p = 0
+    if (storeindex <= 0):
+        log("[Logging...] 找不到签名文件", True)
+        sys.exit()
+    keystorefile = storefiles[int(p) - 1]
+    log("[Logging...] 签名文件 : %s" % keystorefile, True)
+    index = keystorefile.rfind(".keystore")
+    filename = keystorefile[0:index]
+    splits = filename.split("_")
+    if (len(splits) < 3):
+        log("[Logging...] 无法获取签名文件信息,请重命名签名文件格式 <alias_pwd_123456.keystore>", True)
+        sys.exit()
+    keystorealias = splits[0]
+    keystorepass = splits[2]
+    keyaliaspass = splits[2]
+
+    if (keystorealias == "" or keystorepass == ""):
+        log("[Error...] keystorealias or keystorepass is empty")
+        sys.exit()
+
+    retcode = subprocess.call([keytool, "-list", "-keystore", dir + seperater + keystorefile, "-storepass", keystorepass], stdout=subprocess.PIPE)
+    if (retcode != 0):
+        log("[Logging...] 签名文件不正确", True)
+        sys.exit()
+    keystoreinfo = []
+    keystoreinfo.append(keystorefile)
+    keystoreinfo.append(keystorepass)
+    keystoreinfo.append(keystorealias)
+    keystoreinfo.append(keyaliaspass)
+    return keystoreinfo
+
+
+testSign = False
+if (len(sys.argv) < 2):
+    log("[Logging...] 缺少参数, %s [-t] <src_apk>" % sys.argv[0], True);
+    sys.exit()
+opts, args = getopt.getopt(sys.argv[1:], "t")
+for op, value in opts:
+    if (op == "-t"):
+        testSign = True
+
+for file in args :
+    if (os.path.isdir(file)):
+        listfiles = os.listdir(file)
+        for apkfile in listfiles :
+            apkpath = file + seperater + apkfile
+            exec_sign_process(apkpath, testSign)
+    else:
+        exec_sign_process(file, testSign)
+
