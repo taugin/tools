df34813f5b15518cac88a51b65aa7f3c5c6a35d4
taugin <taugin@163.com>
2015-07-06 16:34:11 +0800

    1. Add arguement -c for check dup modified:   mergeapk/mergeapk.py

--- mergeapk/mergeapk.py
+++ mergeapk/mergeapk.py
@@ -7,6 +7,7 @@ import platform
 import subprocess
 import msvcrt
 import shutil
+import getopt
 
 import decompile_apk
 import compile_apk
@@ -18,6 +19,7 @@ import copy_fromapk
 
 TRY_CONFIG = "mergeapk.tryagain"
 SIGNAPK_FILE = os.path.join(os.path.dirname(sys.argv[0]), "..", "signtool", "signapk.py")
+ONLY_CHECK_DUP = False
 
 def log(str, show=True):
     if (show):
@@ -27,8 +29,18 @@ def log(str, show=True):
 if (len(sys.argv) < 3):
     log("[Logging...] 缺少参数: %s apk1 apk2" % os.path.basename(sys.argv[0]), True);
     sys.exit()
-gameapk = os.path.abspath(sys.argv[1])
-payapk = os.path.abspath(sys.argv[2])
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "c")
+    for op, value in opts:
+        if (op == "-c"):
+            ONLY_CHECK_DUP = True
+except getopt.GetoptError as err:
+    log(err)
+    sys.exit()
+
+gameapk = os.path.abspath(args[0])
+payapk = os.path.abspath(args[1])
 
 (name, ext) = os.path.splitext(gameapk)
 gamefolder = name
@@ -44,6 +56,7 @@ def signapk_use_testkey(apkloaderfile):
         return True
     else:
         return False
+
 def clean_tmp_folders(gamefolder, payfolder):
     log("[Logging...] 清除临时文件")
     shutil.rmtree(payfolder, ignore_errors = True)
@@ -53,15 +66,16 @@ functions = []
 functions += ["decompile_apk.apk_decompile(gameapk, gamefolder)"]
 functions += ["decompile_apk.apk_decompile(payapk, payfolder)"]
 functions += ["check_dup.check_dup(gamefolder, payfolder)"]
-functions += ["rebuild_ids.rebuild_ids(gamefolder, payfolder)"]
-functions += ["copy_res.copy_res(gamefolder, payfolder)"]
-functions += ["merge_xml.merge_xml(gamefolder, payfolder)"]
-functions += ["compile_apk.apk_compile(gamefolder, gamemergedapk)"]
-functions += ["copy_fromapk.copy_fromapk(gamemergedapk, gameapk, payapk)"]
-functions += ["clean_tmp_folders(gamefolder, payfolder)"]
 
-if (platform.system().lower() == "windows"):
-    functions += ["signapk_use_testkey(gamemergedapk)"]
+if (ONLY_CHECK_DUP == False):
+    functions += ["rebuild_ids.rebuild_ids(gamefolder, payfolder)"]
+    functions += ["copy_res.copy_res(gamefolder, payfolder)"]
+    functions += ["merge_xml.merge_xml(gamefolder, payfolder)"]
+    functions += ["compile_apk.apk_compile(gamefolder, gamemergedapk)"]
+    functions += ["copy_fromapk.copy_fromapk(gamemergedapk, gameapk, payapk)"]
+    functions += ["clean_tmp_folders(gamefolder, payfolder)"]
+    if (platform.system().lower() == "windows"):
+        functions += ["signapk_use_testkey(gamemergedapk)"]
 
 result = False
 length = len(functions)
@@ -80,9 +94,13 @@ for item in range(0, length):
     if (item >= func_exec_pos):
         log("--------------------------------------------")
         result = eval(functions[item])
-        if (result == False):
+        if (result == False and ONLY_CHECK_DUP == False):
             savestr = '{"function_pos":%d,"filename":r"%s"}' % (item, os.path.abspath(gamemergedapk))
             fd = open(TRY_CONFIG, "w")
             fd.write(savestr)
             fd.close()
             break
+if (ONLY_CHECK_DUP):
+    log("--------------------------------------------")
+    clean_tmp_folders(gamefolder, payfolder)
+
