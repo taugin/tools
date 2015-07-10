6b03011f8da469ab76086d160e36e8173a410606
taugin <taugin@163.com>
2015-07-10 11:13:09 +0800

    Add extra log

--- mergeapk/mergeapk.py
+++ mergeapk/mergeapk.py
@@ -43,7 +43,7 @@ def signapk_use_testkey(apkloaderfile):
 def clean_tmp_folders(gamefolder, payfolder):
     log("[Logging...] 清除临时文件")
     shutil.rmtree(payfolder, ignore_errors = True)
-    log("[Logging...] 临时文件清除完成")
+    log("[Logging...] 临时文件清除完成\n")
 
 def mergeapk_batch(gameapk, payapk, output, newpkgname, company):
     (gamename, ext) = os.path.splitext(gameapk)
@@ -126,6 +126,7 @@ def merge_according_config():
         package = item.get("package")
         company = item.get("company")
         if (gameapk != None and gameapk != "" and payapk != None and payapk != ""):
+            log("[Logging...] ================================================")
             log("[Logging...] 处理合并APK")
             mergeapk_batch(gameapk, payapk, output, package, company)
         else:
