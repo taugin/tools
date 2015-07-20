641e97d6c9e9fe52bc546142faa5f8273055b629
taugin <taugin@163.com>
2015-07-20 14:31:20 +0800

    1. Add pullapk function new file:   pullapk/pullapk.py

---
+++ pullapk/pullapk.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# coding: UTF-8
+import sys
+import os
+import subprocess
+
+def log(str, show=True):
+    if (show):
+        print(str)
+
+def getpackage():
+    cmdlist = ["adb", "shell", "dumpsys", "activity", "top"]
+    p = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
+    #p.wait()
+    package = None
+    if (p != None):
+        for line in p.stdout.readlines():
+            string = line.decode().replace("\n", "")
+            string = string.lstrip()
+            if (string.startswith("ACTIVITY")):
+                list = string.split(" ")
+                if (list != None and len(list) > 1):
+                    list = list[1].split("/")
+                    if (list != None and len(list) > 0):
+                        package = list[0]
+                        log("[Logging...] 顶层APK包名 : [%s]" % package)
+    return package
+
+def getapkfile(package):
+    cmdlist = ["adb", "shell", "pm", "list", "packages", "-f", package]
+    p = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
+    apkfile = None
+    if (p != None):
+        for line in p.stdout.readlines():
+            string = line.decode().replace("\n", "")
+            string = string.strip()
+            if (string.startswith("package:")):
+                string = string[len("package:"):-(len(package) + 1)]
+                log("[Logging...] 顶层APK文件 : [%s]" % string)
+                apkfile = string
+    return apkfile
+
+def pullspecapk(apkfile):
+    if (apkfile != None and apkfile != ""):
+        log("[Logging...] 正在获取APK")
+        cmdlist = ["adb", "pull", apkfile]
+        subprocess.call(cmdlist)
+
+def pullapk():
+    package = getpackage()
+    apkfile = getapkfile(package)
+    pullspecapk(apkfile)
+    log(apkfile)
+
+pullapk()
