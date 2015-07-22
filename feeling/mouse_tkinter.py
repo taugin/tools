af606cd3a130f144cd08267579d586c55305b9f4
taugin <taugin@163.com>
2015-07-22 14:56:42 +0800

    Code Optimize

--- feeling/mouse_tkinter.py
+++ feeling/mouse_tkinter.py
@@ -9,12 +9,16 @@ import subprocess
 
 
 #事件定义
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 TRACKING_ID = 0
-EV_DEVICE = "/dev/input/event0"
+EV_TOUCH_DEVICE = "/dev/input/event0"
 ABS_MT_POSITION_X = "%d" % int(eval("0x35"))
 ABS_MT_POSITION_Y = "%d" % int(eval("0x36"))
 
 ABS_MT_TRACKING_ID = "%d" % int(eval("0x39"))
+ABS_MT_PRESSURE = "%d" % int(eval("0x3a"))
+ABS_MT_TOUCH_MAJOR = "%d" % int(eval("0x30"))
+
 EV_ABS = "%d" % int(eval("0x3"))
 
 EV_KEY = "%d" % int(eval("0x1"))
@@ -26,71 +30,102 @@ UP = "%d" % int(eval("0x0"))
 
 EV_SYN = "%d" % int(eval("0x0"))
 SYN_REPORT = "%d" % int(eval("0x0"))
-
+#======================================================
+scale = 1
 pressing = False
-SCREEN_ORIENTATION = 0
-if (len(sys.argv) > 1):
-    SCREEN_ORIENTATION = 1
+WIDTH = 0
+HEIGHT = 0
 
 def log(str, show=True):
     if (show):
         #top.title(str)
         print(str)
 
+def getoriention():
+    global WIDTH
+    global HEIGHT
+    if (WIDTH > HEIGHT):
+        return "landscape"
+    return "portrait"
+
 def getgeometry():
-    w = 0
-    h = 0
-    if (SCREEN_ORIENTATION == 0):
-        w = 540
-        h = 960
-    else:
-        w = 960
-        h = 540
+    global WIDTH
+    global HEIGHT
+    w = WIDTH
+    h = HEIGHT
+    log("WIDTH : [%s]" % WIDTH)
+
     ws = top.winfo_screenwidth()  
-    hs = top.winfo_screenheight()  
+    hs = top.winfo_screenheight() - 70  
     # calculate position x, y  
     x = (ws/2) - (w/2)
     y = (hs/2) - (h/2)
     geometry = '%dx%d+%d+%d' % (w, h, x, y)
     return geometry
 
-def recvfrom():
+def recvfrom(list):
+    global WIDTH
+    global HEIGHT
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        host_addr = ("127.0.0.1", 8988)
-        s.bind(host_addr)
         log("waiting for data from phone ...")
-        data, (addr, port) = s.recvfrom(1024)
-        log("addr : " + addr)
+        try :
+            data= list.recv(1024)
+        except:
+            log("except")
+            break
+        data = data.decode()
+        if (data == None or data == ""):
+            break
+        data = eval(data)
+        log(data)
+        if (data["command"] == "response_screensize"):
+            WIDTH = data["w"]
+            HEIGHT = data["h"]
+            reset_frame()
+        import time
+        time.sleep(1)
 
-class CommonSocket:
+class TcpSocket:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(("127.0.0.1", 8989))
+        list = [self.s]
+        log("启动线程")
+        th = threading.Thread(target=recvfrom, args=(list))
+        th.setDaemon(True)
+        th.start()
+    def senddata(self, data):
+        self.s.send(data.encode("utf-8"))
+
+class UdpSocket:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.connect(("10.0.1.114", 8989))
-        log("启动线程")
-        th = threading.Thread(target=recvfrom, args=())
-        #th.start()
+        self.s.connect(("10.0.1.114", 8990))
+
     def senddata(self, data):
         self.s.sendall(data.encode("utf-8"))
 
 class ScreenTranform:
+    HEIGHT = 540
     def __init__(self):
-        pass
+        dict = {}
+        dict["command"] = "request_screensize"
+        tcp_socket.senddata(str(dict))
 
     def getpositionx(self):
-        if (SCREEN_ORIENTATION == 0):
+        if (getoriention() == "portrait"):
             return ABS_MT_POSITION_X
         return ABS_MT_POSITION_Y
 
     def getpositiony(self):
-        if (SCREEN_ORIENTATION == 0):
+        if (getoriention() == "portrait"):
             return ABS_MT_POSITION_Y
         return ABS_MT_POSITION_X
 
     def process_position(self, event):
-        if (event.code == self.getpositiony() and SCREEN_ORIENTATION != 0):
+        if (event.code == self.getpositiony() and getoriention() != "portrait"):
             tmp = int(event.value)
-            tmp = 540-tmp
+            tmp = ScreenTranform.HEIGHT-tmp
             event.value = "%d" % tmp
 
 class PhoneEvent:
@@ -103,20 +138,37 @@ class PhoneEvent:
 
 def sendevent(phoneevent):
     cmdlist = ["adb", "shell", "sendevent", phoneevent.device, phoneevent.type, phoneevent.code, phoneevent.value]
-    log(" ".join(cmdlist))
+    string = ""
+    for item in range(0, len(cmdlist)):
+        showstr = cmdlist[item]
+        if (item > 3):
+            showstr = hex(eval(cmdlist[item]))
+            showstr = showstr[2:]
+            if (item == 4 or item == 5):
+                showstr = showstr.rjust(4, '0')
+            else:
+                showstr = showstr.rjust(8, '0')
+        string += showstr + " "
+    #log(" ".join(cmdlist))
+    log(string)
     subprocess.call(cmdlist)
 
 def mousenevent(event):
     #log("[Move] x : %d, y : %d" % (event.x, event.y))
-    data = {"x":event.x, "y":event.y}
-    udpsocket.senddata(str(data))
+    data = {}
+    data["x"] = event.x
+    data["y"] = event.y
+    udp_socket.senddata(str(data))
     if (pressing):
-        phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, screenTransform.getpositionx(), "%d" % event.x)
+        phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, screenTransform.getpositionx(), "%d" % event.x)
         sendevent(phoneevent)
-        phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, screenTransform.getpositiony(), "%d" % event.y)
+        phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, screenTransform.getpositiony(), "%d" % event.y)
         sendevent(phoneevent)
 
-        phoneevent = PhoneEvent(EV_DEVICE, EV_SYN, SYN_REPORT, "0")
+        phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, ABS_MT_PRESSURE, "%d" % 35)
+        sendevent(phoneevent)
+
+        phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
         sendevent(phoneevent)
 
 def mouse_left_down(event):
@@ -124,14 +176,25 @@ def mouse_left_down(event):
     global TRACKING_ID
     TRACKING_ID = TRACKING_ID + 1
     
-    phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, ABS_MT_TRACKING_ID, "%d" % TRACKING_ID)
-    sendevent(phoneevent)
-    phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, screenTransform.getpositionx(), "%d" % event.x)
-    sendevent(phoneevent)
-    phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, screenTransform.getpositiony(), "%d" % event.y)
+    #huawei
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, "1", "%d" % int(eval("0x14a")), "%d" % 1)
     sendevent(phoneevent)
     
-    phoneevent = PhoneEvent(EV_DEVICE, EV_SYN, SYN_REPORT, "0")
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, ABS_MT_TRACKING_ID, "%d" % TRACKING_ID)
+    sendevent(phoneevent)
+
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, screenTransform.getpositionx(), "%d" % event.x)
+    sendevent(phoneevent)
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, screenTransform.getpositiony(), "%d" % event.y)
+    sendevent(phoneevent)
+
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, ABS_MT_PRESSURE, "%d" % 35)
+    sendevent(phoneevent)
+
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, ABS_MT_TOUCH_MAJOR, "%d" % 6)
+    sendevent(phoneevent)
+
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
     sendevent(phoneevent)
 
     pressing = True
@@ -140,20 +203,26 @@ def mouse_left_up(event):
     global pressing
     #log("[Up] x : %d, y : %d, state : %d" % (event.x, event.y, event.state))
     pressing = False
-    phoneevent = PhoneEvent(EV_DEVICE, EV_ABS, ABS_MT_TRACKING_ID, "-1")
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_ABS, ABS_MT_TRACKING_ID, "%d" % 0xffffffff)
     sendevent(phoneevent)
 
-    phoneevent = PhoneEvent(EV_DEVICE, EV_SYN, SYN_REPORT, "0")
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
+    sendevent(phoneevent)
+    
+    #Add for huawei
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, "1", "%d" % int(eval("0x14a")), "%d" % 0)
+    sendevent(phoneevent)
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
     sendevent(phoneevent)
 
 def process_click(key):
-    phoneevent = PhoneEvent(EV_DEVICE, EV_KEY, key, DOWN)
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_KEY, key, DOWN)
     sendevent(phoneevent)
-    phoneevent = PhoneEvent(EV_DEVICE, EV_SYN, SYN_REPORT, "0")
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
     sendevent(phoneevent)
-    phoneevent = PhoneEvent(EV_DEVICE, EV_KEY, key, UP)
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_KEY, key, UP)
     sendevent(phoneevent)
-    phoneevent = PhoneEvent(EV_DEVICE, EV_SYN, SYN_REPORT, "0")
+    phoneevent = PhoneEvent(EV_TOUCH_DEVICE, EV_SYN, SYN_REPORT, "0")
     sendevent(phoneevent)
 
 def mouse_right_click(event):
@@ -170,21 +239,28 @@ def f2_click(event):
         pressing = True
     log("pressing : [%d]" % pressing)
 
-def f12_click(event):
-    global SCREEN_ORIENTATION
-    if (SCREEN_ORIENTATION == 0):
-        SCREEN_ORIENTATION = 1
-    else:
-        SCREEN_ORIENTATION = 0
+def reset_frame():
     top.geometry(getgeometry())
 
 def esc_click(event):
     top.quit()
 
-udpsocket = CommonSocket()
-screenTransform = ScreenTranform()
+def connect_phone():
+    cmdlist = ["adb", "forward", "tcp:8989", "tcp:8989"]
+    ret = subprocess.call(cmdlist)
+    if (ret != 0):
+        sys.exit(0)
+
 
 top = tkinter.Tk()
+
+connect_phone()
+tcp_socket = TcpSocket()
+udp_socket = UdpSocket()
+
+screenTransform = ScreenTranform()
+
+
 top.resizable(False,False)
 top.geometry(getgeometry())
 top.bind("<Motion>", mousenevent)
@@ -193,8 +269,8 @@ top.bind("<ButtonRelease-1>", mouse_left_up)
 top.bind("<Button-2>", mouse_middle_click)
 top.bind("<Button-3>", mouse_right_click)
 top.bind("<Key-F2>", f2_click)
-top.bind("<Key-F12>", f12_click)
+#top.bind("<Key-F12>", f12_click)
 top.bind("<Key-Escape>", esc_click)
 
 top.mainloop()
-
+log("quit")
