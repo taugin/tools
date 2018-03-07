11db6402ccda02a206bbe8da51485cb7b663a502
taugin <taugin@163.com>
2018-03-07 18:17:07 +0800

    1. 回编译自动签名

--- script/base/apktool.py
+++ script/base/apktool.py
@@ -9,15 +9,38 @@ COM_DIR = os.path.normpath(COM_DIR)
 sys.path.append(COM_DIR)
 
 import Common
+import getopt
 import Log
-
 import subprocess
 
+SIGNAPK_FILE = os.path.join(os.path.dirname(sys.argv[0]), "signapk.py")
 
 def apktool_cmd():
     cmdlist = [Common.JAVA, "-jar", Common.APKTOOL_JAR]
     cmdlist += sys.argv[1:]
     subprocess.call(cmdlist)
 
+def signapk(dstapk):
+    Log.out("")
+    cmdlist = ["python", SIGNAPK_FILE, dstapk]
+    ret = subprocess.call(cmdlist)
+    if (ret == 0):
+        return True
+    else:
+        return False
+
 apktool_cmd()
+
+#回编译签名
+if (len(sys.argv) > 1 and sys.argv[1] == "b"):
+    apkfile = None
+    opts, args = getopt.getopt(sys.argv[3:], "o:")
+    for op, value in opts:
+        if (op == "-o"):
+            apkfile = value
+    if (apkfile != None and len(apkfile) > 0) :
+        if (signapk(apkfile) == True):
+            if (os.path.exists(apkfile)):
+                os.remove(apkfile)
+
 Common.pause()
