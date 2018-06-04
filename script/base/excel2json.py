c7420dd86bc660711351c6045a1bc85696eb515d
taugin <taugin@163.com>
2018-06-04 10:46:59 +0800

    1. 优化excel2json 2. 增加类型信息 3. 增加描述信息

--- script/base/excel2json.py
+++ script/base/excel2json.py
@@ -9,19 +9,37 @@ COM_DIR = os.path.join(DIR, "..", "common")
 COM_DIR = os.path.normpath(COM_DIR)
 sys.path.append(COM_DIR)
 
+import json
 import xlrd
 import Log
 import Common;
 
-ADPLACES = "adplaces"
-ADNAME = "name"
-ADPIDS = "pids"
-def tran2int(number):
+#描述广告位名称
+AD_PLACES = "adplaces"
+#描述表单之间关联的值
+AD_NAME = "name"
+#描述所有广告位
+AD_PIDS = "pids"
+#表示表头的行数
+HEADER_ROWS_COUNT = 3
+#表示内容开始的位置
+CONTENT_START_POS = 3
+#表示健值的表头位置
+HEADER_KEY_POS = 0
+#表示类型的健值位置
+HEADER_TYPE_POS = 1;
+#表示描述信息的健值位置
+HEADER_DESC_POS = 2
+
+def format_value(origin, col_type):
     try:
-        return int(number)
+        if col_type == "int":
+            return int(origin)
+        if col_type == "float":
+            return float(origin)
     except:
         pass
-    return number;
+    return origin
 
 def read_rows(sheet_table, row):
     '''Read row values'''
@@ -62,24 +80,25 @@ def open_excel(excel_file):
     return None
 
 def parse_pidlist(pids_sheet, pids_map):
-    if pids_sheet == None or pids_sheet.nrows < 2:
+    if pids_sheet == None or pids_sheet.nrows < HEADER_ROWS_COUNT + 1:
         return None
-    row_header = read_rows(pids_sheet, 0)
-    pids_count = pids_sheet.nrows - 1
-    for row in range(1, pids_count + 1):
+    header_row_key = read_rows(pids_sheet, HEADER_KEY_POS)
+    header_row_type = read_rows(pids_sheet, HEADER_TYPE_POS)
+    pids_count = pids_sheet.nrows - HEADER_ROWS_COUNT
+    for row in range(CONTENT_START_POS, pids_count + HEADER_ROWS_COUNT):
         row_value = read_rows(pids_sheet, row)
         pid = {}
-        place_name = row_value[find_index(row_header, ADNAME)]
+        place_name = row_value[find_index(header_row_key, AD_NAME)]
         has_empty_value = False
-        for col in row_header:
-            if col == ADNAME:
+        for col in header_row_key:
+            if col == AD_NAME:
                 continue
-            pid[col] = row_value[find_index(row_header, col)]
+            pid[col] = row_value[find_index(header_row_key, col)]
+            col_type = header_row_type[find_index(header_row_key, col)]
             if pid[col] == None or len(str(pid[col])) <= 0:
                 has_empty_value = True
                 continue
-            if type(pid[col]) == float:
-                pid[col] = tran2int(pid[col])
+            pid[col] = format_value(pid[col], col_type)
         if has_empty_value:
             continue
         if place_name in pids_map:
@@ -88,32 +107,34 @@ def parse_pidlist(pids_sheet, pids_map):
             pids_map[place_name] = [pid]
 
 def find_index(header_list, name):
+    '''查找key值所在的index'''
     if header_list == None or len(header_list) <= 0:
         return -1
-    return header_list.index(name)
+    if name in header_list:
+        return header_list.index(name)
+    return None
 
 #生成adplace
 def generate_adplace(adplaces_sheet, adplaces):
-    if adplaces_sheet == None or adplaces_sheet.nrows < 2:
+    if adplaces_sheet == None or adplaces_sheet.nrows < HEADER_ROWS_COUNT + 1:
         return None
-    row_header = read_rows(adplaces_sheet, 0)
-    adplaces_count = adplaces_sheet.nrows - 1
-    for row in range(1, adplaces_count + 1):
+    header_row_key = read_rows(adplaces_sheet, HEADER_KEY_POS)
+    header_row_type = read_rows(adplaces_sheet, HEADER_TYPE_POS)
+    adplaces_count = adplaces_sheet.nrows - HEADER_ROWS_COUNT
+    for row in range(HEADER_ROWS_COUNT, adplaces_count + HEADER_ROWS_COUNT):
         row_value = read_rows(adplaces_sheet, row)
         adplace = {}
         has_empty_value = False
-        for col in row_header:
-            adplace[col] = row_value[find_index(row_header, col)]
-            if adplace[col] == None or (len(str(adplace[col])) <= 0):
+        for col_key in header_row_key:
+            adplace[col_key] = row_value[find_index(header_row_key, col_key)]
+            col_type = header_row_type[find_index(header_row_key, col_key)]
+            if adplace[col_key] == None or (len(str(adplace[col_key])) <= 0):
                 has_empty_value = True
+                Log.out("[Logging...] 发现空白字段: [%s]" % col_key)
                 continue
-            if type(adplace[col]) == float:
-                adplace[col] = tran2int(adplace[col])
+            adplace[col_key] = format_value(adplace[col_key], col_type)
         if not has_empty_value:
             adplaces.append(adplace)
-
-def find_pids_in_map(pids_map, name):
-    return pids_map[name]
 
 def read_excel(excel_file):
     adconfig = {}
@@ -123,18 +144,21 @@ def read_excel(excel_file):
         Log.out("[Logging...] 无法定位文件 : [%s]" % excel_file)
         Common.pause()
         sys.exit(0)
+
     excel_obj = open_excel(excel_file)
     if not excel_obj:
         Log.out("[Logging...] 无法解析文件 : [%s]" % excel_file)
         Common.pause()
         sys.exit(0)
+
     sheet_names = read_sheet_names(excel_obj)
     if not sheet_names:
         Log.out("[Logging...] 无法解析表单 : [%s]" % excel_file)
         Common.pause()
         sys.exit(0)
+
     #获取adplaces工作表
-    adplace_sheet = read_sheet_by_name(excel_obj, ADPLACES)
+    adplace_sheet = read_sheet_by_name(excel_obj, AD_PLACES)
     if not adplace_sheet:
         Log.out("[Logging...] 无法获取表单 : [%s]" % "adplaces")
         Common.pause()
@@ -143,16 +167,16 @@ def read_excel(excel_file):
     #获取广告位数据
     generate_adplace(adplace_sheet, adplaces)
     for name in sheet_names:
-        if name == ADPLACES:
+        if name == AD_PLACES:
             continue
         #获取各个广告平台配置数据
         parse_pidlist(read_sheet_by_name(excel_obj, name), pids_map)
 
-    adconfig[ADPLACES] = adplaces;
+    adconfig[AD_PLACES] = adplaces;
 
     for adplace in adplaces:
-        name = adplace[ADNAME]
-        adplace[ADPIDS] = pids_map[name]
+        name = adplace[AD_NAME]
+        adplace[AD_PIDS] = pids_map[name]
 
     adstring = str(adconfig)
     adstring = adstring.replace("\'", "\"")
@@ -161,8 +185,13 @@ def read_excel(excel_file):
     name, ext = os.path.splitext(basename)
     newname = name + ".txt"
     newfile = os.path.join(dirname, newname)
+    output = str(adstring)
+    try:
+        output = json.dumps(adconfig, indent=4)
+    except:
+        output = str(adstring)
     f = open(newfile, "w")
-    f.write(str(adstring))
+    f.write(output)
     f.close()
     Log.out("[Logging...] 文件转换成功 : [%s]" % newfile)
     Common.pause()
