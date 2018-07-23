c9f74dc41b10ac8a7d7c2df364f9eb31f82bdad5
taugin <taugin@163.com>
2018-07-23 15:15:02 +0800

    1. 增加设备选择

--- script/base/pullapk.py
+++ script/base/pullapk.py
@@ -11,13 +11,50 @@ sys.path.append(COM_DIR)
 import Common
 import Log
 
-import os
+import re
 import subprocess
 import time
 
+SELECT_DEVICE = None
+
+def input_no(start, end):
+    while True:
+        i = input("请输入设备顺序 : ")
+        if i != None and i.isdigit() and int(i) >= start and int(i) <= end:
+            return i
+    return 1
+
+def get_select_devices():
+    try:
+        cmd = [Common.ADB, "devices", "-l"]
+        devices = []
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        p.wait(5)
+        allLines = p.stdout.readlines()
+        for s in allLines:
+            s = str(s, "utf-8")
+            s = s.replace("\r", "")
+            s = s.replace("\n", "")
+            if (s.startswith("List") or len(s) == 0):
+                continue
+            devices.append([re.split("\s+", s)[0], s])
+        if (len(devices) > 1):
+            Log.out("发现的设备列表")
+            for index in range(0, len(devices)):
+                Log.out("%s : %s" % (index + 1, devices[index][1]))
+            no = input_no(1, len(devices))
+            return devices[int(no) - 1][0]
+        elif(len(devices) == 1):
+            return devices[0][0]
+    except:
+        pass
+    return None
 
 def getpackage():
-    cmdlist = [Common.ADB, "-d", "shell", "dumpsys", "activity", "top"]
+    if (SELECT_DEVICE != None and len(SELECT_DEVICE) > 0) :
+        cmdlist = [Common.ADB, "-s", SELECT_DEVICE, "shell", "dumpsys", "activity", "top"]
+    else:
+        cmdlist = [Common.ADB, "-d", "shell", "dumpsys", "activity", "top"]
     p = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
     # p.wait()
     pkglist = []
@@ -36,7 +73,10 @@ def getpackage():
     return package
 
 def getapkfile(package):
-    cmdlist = [Common.ADB, "-d", "shell", "pm", "path", package]
+    if (SELECT_DEVICE != None and len(SELECT_DEVICE) > 0) :
+        cmdlist = [Common.ADB, "-s", SELECT_DEVICE, "shell", "pm", "path", package]
+    else:
+        cmdlist = [Common.ADB, "-d", "shell", "pm", "path", package]
     p = subprocess.Popen(cmdlist, stdout=subprocess.PIPE)
     apkfile = None
     if (p != None):
@@ -58,7 +98,10 @@ def pullspecapk(apkfile, package):
         tempFile = os.path.normpath(tempFile)
         f = open(tempFile, "wb")
         f.close()
-        cmdlist = [Common.ADB, "-d", "pull", apkfile, tempFile]
+        if (SELECT_DEVICE != None and len(SELECT_DEVICE) > 0) :
+            cmdlist = [Common.ADB, "-s", SELECT_DEVICE, "pull", apkfile, tempFile]
+        else:
+            cmdlist = [Common.ADB, "-d", "pull", apkfile, tempFile]
         subprocess.call(cmdlist)
         if not os.path.exists(tempFile):
             return
@@ -94,6 +137,8 @@ def getlabel(apkFile):
     return label
 
 def pullapk():
+    global SELECT_DEVICE
+    SELECT_DEVICE = get_select_devices()
     package = getpackage()
     if (package != None):
         apkfile = getapkfile(package)
