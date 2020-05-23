1e308e88ffdb5af09b982116c3b01df77f89564a
taugin <taugin@163.com>
2020-05-23 11:20:48 +0800

    1. bat文件优化

--- bin/apkmerge.bat
+++ bin/apkmerge.bat
@@ -1,9 +1,10 @@
 @echo off
-set  pythonpath=C:\Users\liuzhao.wei\AppData\Local\Programs\Python\Python38-32\python.exe
-@rem for /F %%i in ('where python') do ( set pythonpath=%%i)
+@rem set  pythonpath=C:\Users\liuzhao.wei\AppData\Local\Programs\Python\Python38-32\python.exe
+for /F %%i in ('where python') do (set pythonpath=%%i)
 set  apkmerge=script\apkmerger\mergeapk.py
 set  curdir=%~dp0
 set  fullpypath=%curdir%..\%apkmerge%
+echo [Logging...] Python : %pythonpath%
 @rem echo curdir=%curdir%
 @rem echo apkmerge=%apkmerge%
 @rem echo pythonpath=%pythonpath%
