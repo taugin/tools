4d304616e3bee511a735aae421a1d026ea8160d7
taugin <taugin@163.com>
2020-05-24 14:07:33 +0800

    1. bat优化

--- bin/apkmerge.bat
+++ bin/apkmerge.bat
@@ -4,7 +4,7 @@ for /F %%i in ('where python') do (set pythonpath=%%i)
 set  apkmerge=script\apkmerger\mergeapk.py
 set  curdir=%~dp0
 set  fullpypath=%curdir%..\%apkmerge%
-echo [Logging...] Python : %pythonpath%
+echo [Logging...] 执行文件路径 : %pythonpath%
 @rem echo curdir=%curdir%
 @rem echo apkmerge=%apkmerge%
 @rem echo pythonpath=%pythonpath%
