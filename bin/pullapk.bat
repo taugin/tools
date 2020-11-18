45a74b290916454da9187c4799d0496db3701ff5
taugin <taugin@163.com>
2020-11-18 15:57:38 +0800

    1. 修改获取python路径地址

--- bin/pullapk.bat
+++ bin/pullapk.bat
@@ -1,6 +1,10 @@
 @echo off
 @rem set  pythonpath=C:\Users\liuzhao.wei\AppData\Local\Programs\Python\Python38-32\python.exe
-for /F %%i in ('where python') do (set pythonpath=%%i)
+for /F %%i in ('where python') do (
+		set pythonpath=%%i
+		goto start
+	)
+:start
 set  pythoncode=script\base\pullapk.py
 set  curdir=%~dp0
 set  fullpypath=%curdir%..\%pythoncode%
