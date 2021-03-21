c9d27c29b5b4b154c36129e85173566611ffa0c0
taugin <taugin@163.com>
2021-03-21 23:30:19 +0800

    1. 修复带空格的路径导致的解压失败

--- bin/unapk.bat
+++ bin/unapk.bat
@@ -1,14 +1,27 @@
 @echo off
 set  curdir=%~dp0
-set  filepath=%~nx1
+set  filename=%~nx1
 set  cmd7zfile=%curdir%7z.exe
-set  filename=%filepath:~0,-4%
-echo curdir : %curdir%, filepath : %filepath% , 7zfile : %cmd7zfile%, cwd : %cd% , filename : %filename%
+set  filedir=%filename:~0,-4%
+@rem echo curdir : %curdir%, filename : %filename% , 7zfile : %cmd7zfile%, cwd : %cd% , filedir : %filedir%
 echo [Logging...] 脚本文件路径 : [%cmd7zfile%]
-echo [Logging...] 脚本代码路径 : [%filepath%]
-set command=%cmd7zfile% x -tzip -bd -y -o%filename% %filepath%
+echo [Logging...] 脚本代码路径 : [%filename%]
+set command=%cmd7zfile% x -tzip -bd -y -o"%filedir%" "%filename%"
 echo [Logging...] 脚本命令详情 : [%command%]
-%command% > nul
-if 0 == %ERRORLEVEL% (ECHO [Logging...] 文件解压成功) else (ECHO [Logging...] 文件解压失败)
-echo [Logging...] Wait for 3 seconds to exit
-ping localhost -n 5 > nul
+SET TMPFOLDER=%TMP%
+SET temp_extract_result_file=%TMPFOLDER%\temp_extract_result_file.txt
+@rem echo [Logging...] 临时文件名称 : [%temp_extract_result_file%]
+%command% > %temp_extract_result_file%
+if 0 == %ERRORLEVEL% (
+    del %temp_extract_result_file%
+    ECHO [Logging...] 文件解压成功
+    echo [Logging...] Wait for 3 seconds to exit
+    ping localhost -n 5 > nul
+) else (
+    ECHO [Logging...] 文件解压失败
+    for /F "delims=" %%i in ('TYPE %temp_extract_result_file%') do (
+        ECHO [Warning...] %%i
+	)
+    del %temp_extract_result_file%
+    pause
+)
