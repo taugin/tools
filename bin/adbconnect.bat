14511331986331b0fb5f59e68b7e39e072791fc7
taugin <taugin@163.com>
2022-10-28 19:57:22 +0800

    1. 优化获取IP地址

--- bin/adbconnect.bat
+++ bin/adbconnect.bat
@@ -49,16 +49,30 @@ echo [Logging...] Wait for usb device connected : [%waitdevice%]
 %waitdevice%
 echo [Logging...] Adb connected successfully
 rem wlan的信息
+set IP_ADDR=
+rem #########################################################################
+IF not "%IP_ADDR%" == "" goto :connect_ip_address
 for /F "delims=" %%i in ('adb -d shell ip addr show wlan0 ^| findstr /c:"inet "') do (set WLAN_INFO=%%i)
-rem echo WLAN_INFO=%WLAN_INFO%
-
-rem 获取手机ip地址信息
 for /F "tokens=2 delims= " %%i in ("%WLAN_INFO%") do (set IP_ADDR_MASK=%%i)
-rem echo IP_ADDR_MASK=%IP_ADDR_MASK%
-
-rem 获取手机ip地址信息
 for /F "tokens=1 delims=/" %%i in ("%IP_ADDR_MASK%") do (set IP_ADDR=%%i)
-rem echo IP_ADDR=%IP_ADDR%
+rem #########################################################################
+IF not "%IP_ADDR%" == "" goto :connect_ip_address
+for /F "delims=" %%i in ('adb -d shell netcfg ^| findstr "wlan"') do (set WLAN_INFO=%%i)
+for /F "tokens=3 delims= " %%i in ("%WLAN_INFO%") do (set IP_ADDR_MASK=%%i)
+for /F "tokens=1 delims=/" %%i in ("%IP_ADDR_MASK%") do (set IP_ADDR=%%i)
+rem #########################################################################
+IF not "%IP_ADDR%" == "" goto :connect_ip_address
+for /F "delims=" %%i in ('adb -d shell ifconfig ^| findstr "inet addr:"') do (
+	set WLAN_INFO=%%i
+	goto :parse_ifconfig
+)
+:parse_ifconfig
+for /F "tokens=2 delims=:" %%i in ("%WLAN_INFO%") do (set IP_ADDR_MASK=%%i)
+for /F "tokens=1 delims= " %%i in ("%IP_ADDR_MASK%") do (set IP_ADDR=%%i)
+rem #########################################################################
+
+
+:connect_ip_address
 echo [Logging...] Current Phone IP Address : [%IP_ADDR%]
 set ADB_PORT=9999
 set adb_tcpip=adb -d tcpip %ADB_PORT%
