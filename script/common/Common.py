bbba195dfea90874dca28519f4b9e5f9ea2b074e
taugin <taugin@163.com>
2022-12-13 15:47:25 +0800

    1. 更新apktool版本

--- script/common/Common.py
+++ script/common/Common.py
@@ -40,7 +40,7 @@ SEPERATER = os.path.sep
 
 #########################jar包文件路径定义#################################
 #APKTOOL的jar包
-APKTOO_JAR_VERSION = "2.6.2-snapshot"
+APKTOO_JAR_VERSION = "2.7.0"
 APKTOOL_JAR = os.path.join(LIB_DIR, "apktool_%s.jar" % APKTOO_JAR_VERSION)
 
 #签名jar
