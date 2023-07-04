03c056df5600455a14e0d1960f34416f338f073f
taugin <taugin@163.com>
2023-07-04 19:08:14 +0800

    1. 更新获取顶层activity的方式

--- bin/dumpapp.bat
+++ bin/dumpapp.bat
@@ -4,11 +4,11 @@
 echo [Logging...] ARGS : [%*]
 echo [Logging...] %date% %time%
 rem 获取顶层应用
-for /F "delims=" %%i in ('adb shell dumpsys activity top ^| findstr "ACTIVITY"') do (set ALL_ACTIVITY=%%i)
+for /F "delims=" %%i in ('adb shell dumpsys activity activities ^| findstr "mFocusedWindow"') do (set ALL_ACTIVITY=%%i)
 rem echo ALL_ACTIVITY=%ALL_ACTIVITY%
 
 rem 获取包名
-for /F "tokens=2 delims= " %%i in ("%ALL_ACTIVITY%") do (set TOP_ACTIVITY=%%i)
+for /F "tokens=3 delims= " %%i in ("%ALL_ACTIVITY%") do (set TOP_ACTIVITY=%%i)
 rem echo TOP_ACTIVITY=%TOP_ACTIVITY%
 
 rem 获取包名
@@ -19,6 +19,9 @@ rem 获取类名
 for /F "tokens=2 delims=/" %%i in ("%TOP_ACTIVITY%") do (set ACTIVITY_NAME=%%i)
 rem echo ACTIVITY_NAME=%ACTIVITY_NAME%
 
+for /F "tokens=1 delims=}" %%i in ("%ACTIVITY_NAME%") do (set ACTIVITY_NAME=%%i)
+rem echo ACTIVITY_NAME=%ACTIVITY_NAME%
+
 rem 获取进程数据
 for /F "tokens=1 delims=" %%i in ('adb shell ps -A ^| findstr "%PACKAGE_NAME%"') do (set PS_LINE=%%i)
 rem echo PS_LINE=%PS_LINE%
@@ -27,7 +30,9 @@ rem 获取用户ID
 for /F "tokens=1 delims= " %%i in ("%PS_LINE%") do (set USER_ID=%%i)
 rem echo USER_ID=%USER_ID%
 
-echo [Logging...] USER_ID : [%USER_ID%] , PACKAGE_NAME : [%PACKAGE_NAME%] , ACTIVITY_NAME : [%ACTIVITY_NAME%]
+echo [Logging...] USER_ID : [%USER_ID%]
+echo [Logging...] PACKAGE_NAME : [%PACKAGE_NAME%]
+echo [Logging...] ACTIVITY_NAME : [%ACTIVITY_NAME%]
 
 adb shell ps -A | findstr "%USER_ID%"
 rem if not "%ERRORLEVEL%" == "0" (
@@ -39,4 +44,5 @@ rem )
 echo.
 rem choice /t 1 /d y /n >nul
 ping localhost -n 2 > nul
+echo =================
 if "%1" == "loop" (goto start)
--- bin/dumplog.bat
+++ bin/dumplog.bat
@@ -2,11 +2,11 @@
 rem 获取顶层应用
 setlocal
 rem 获取顶层应用
-for /F "delims=" %%i in ('adb shell dumpsys activity top ^| findstr "ACTIVITY"') do (set ALL_ACTIVITY=%%i)
+for /F "delims=" %%i in ('adb shell dumpsys activity activities ^| findstr "mFocusedWindow"') do (set ALL_ACTIVITY=%%i)
 rem echo ALL_ACTIVITY=%ALL_ACTIVITY%
 
 rem 获取包名
-for /F "tokens=2 delims= " %%i in ("%ALL_ACTIVITY%") do (set TOP_ACTIVITY=%%i)
+for /F "tokens=3 delims= " %%i in ("%ALL_ACTIVITY%") do (set TOP_ACTIVITY=%%i)
 rem echo TOP_ACTIVITY=%TOP_ACTIVITY%
 
 rem 获取包名
