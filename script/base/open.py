069eb146746cb034dba309139972253245891e4a
taugin <taugin@163.com>
2023-09-13 10:44:02 +0800

    1. 优化amstart

--- script/base/open.py
+++ script/base/open.py
@@ -22,7 +22,7 @@ def open_google_play(args):
         package_name = args[0]
         install_referrer="https://play.google.com/store/apps/details?id={}&referrer=utm_source%3Dgoogle%26utm_medium%3Dcpc%26utm_term%3Dshoes%26utm_content%3Dlogolink%26utm_campaign%3Dspring_sale%26gclid%3Dadegadetwd3aer".format(package_name)
         #install_referrer="https://play.google.com/store/apps/details?id=com.simple.file.tool.browser&referrer=utm_source%3Dgoogle%26utm_medium%3Dcpc%26utm_term%3Daaaaaa%26anid%3Dadmob"
-        print("install_referrer : {}".format(install_referrer))
+        print("[Logging...] 安装引荐网址 : {}".format(install_referrer))
         cmdlist = ['adb','shell','am', 'start','-a', 'android.intent.action.VIEW', '-d', install_referrer]
         subprocess.call(cmdlist)
     else:
