89931163cb4063b297fdd029e438b02f6e91d94b
taugin <taugin@163.com>
2024-07-25 16:51:28 +0800

    1. dlog优化

--- bin/dlog.bat
+++ bin/dlog.bat
@@ -7,11 +7,17 @@ for %%x in (%*) do (
 :logcat
 echo ********* Wait for device connect ...
 adb wait-for-device
+if "%1" == "-c" (
+    echo ********* Clean log cache ...
+    adb logcat -c
+    exit
+)
 if %argc% == 0 (
     adb shell setprop log.tag.hauyu V
     adb shell setprop log.tag.simple V
-    echo ********* adb logcat -s simple hauyu ...
-    adb logcat -s simple hauyu
+    adb shell setprop log.tag.mobsdk V
+    echo ********* adb logcat -s simple hauyu mobsdk ...
+    adb logcat -s simple hauyu mobsdk
 ) else (
     echo ********* adb logcat -s %1 %2 %3 %4 %5 %6 %7 %8 %9
     for %%a in (%*) do (
