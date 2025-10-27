176320c92f25cb92f0e1f35a73a11c71f2a0b7c0
taugin <taugin@163.com>
2025-10-27 16:34:21 +0800

    1. 增加jadx1.5.3

--- bin/jadx-gui.bat
+++ bin/jadx-gui.bat
@@ -2,9 +2,11 @@
 
 set FILE1=%~dp0\jadx\bin\jadx-gui.bat
 set FILE2=%~dp0\jadx_1.5.0_green\jadx-gui-1.5.0.exe
+set FILE3=%~dp0\jadx-gui-1.5.3-with-jre-win\jadx-gui-1.5.3.exe
 
 if exist "%FILE1%" goto run1
 if exist "%FILE2%" goto run2
+if exist "%FILE3%" goto run3
 goto notFound
 
 :run1
@@ -15,6 +17,11 @@ goto end
 :run2
 echo 正在执行 %FILE2%
 call "%FILE2%" %*
+goto end
+
+:run3
+echo 正在执行 %FILE3%
+call "%FILE3%" %*
 goto end
 
 :notFound
