7ed2f6260484889db4f0b5ba70989aebd6cc96e5
taugin <taugin@163.com>
2026-04-27 20:53:47 +0800

    1. 参考dlog的逻辑修改dlog.bat

--- bin/dlog.bat
+++ bin/dlog.bat
@@ -1,29 +1,62 @@
 @echo off
-set argc=0
-for %%x in (%*) do (
-    set /a argc+=1
+setlocal EnableDelayedExpansion
+
+set FLAG=
+if "%1" == "-d" (
+    set FLAG=-d
+    shift
+) else if "%1" == "-e" (
+    set FLAG=-e
+    shift
 )
-@rem echo %argc%
+
+if "%1" == "-c" (
+    call :set_default_flag
+    echo ********* Clean log cache ...
+    adb !FLAG! wait-for-device
+    adb !FLAG! logcat -c
+    shift
+)
+
+call :set_tags %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 :logcat
 echo ********* Wait for device connect ...
-adb wait-for-device
-if "%1" == "-c" (
-    echo ********* Clean log cache ...
-    adb logcat -c
-    exit
-)
-if %argc% == 0 (
-    adb shell setprop log.tag.hauyu V
-    adb shell setprop log.tag.simple V
-    adb shell setprop log.tag.mobsdk V
-    echo ********* adb logcat -s simple hauyu mobsdk ...
-    adb logcat -s simple hauyu mobsdk
+call :set_default_flag
+adb !FLAG! wait-for-device
+if "%1" == "" (
+    adb !FLAG! shell setprop log.tag.hauyu V
+    adb !FLAG! shell setprop log.tag.simple V
+    adb !FLAG! shell setprop log.tag.mobsdk V
+    echo ********* adb !FLAG! logcat -s simple hauyu mobsdk ...
+    adb !FLAG! logcat -s simple hauyu mobsdk
 ) else (
-    echo ********* adb logcat -s %1 %2 %3 %4 %5 %6 %7 %8 %9
-    for %%a in (%*) do (
-        adb shell setprop log.tag.%%a V
+    echo ********* adb !FLAG! logcat -s !TAGS!
+    for %%a in (!TAGS!) do (
+        adb !FLAG! shell setprop log.tag.%%a V
     )
-    adb logcat -s %1 %2 %3 %4 %5 %6 %7 %8 %9
+    adb !FLAG! logcat -s !TAGS!
 )
 
 goto logcat
+
+:set_default_flag
+if not "!FLAG!" == "" goto :eof
+set device_count=0
+for /f "tokens=1,2" %%a in ('adb devices') do (
+    if "%%b" == "device" (
+        set /a device_count+=1
+    )
+)
+if !device_count! GTR 1 (
+    set FLAG=-d
+)
+goto :eof
+
+:set_tags
+if "%~1" == "" (
+    set TAGS=simple hauyu mobsdk
+) else (
+    set TAGS=%*
+)
+goto :eof
